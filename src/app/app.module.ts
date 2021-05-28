78ae56f17dfa8c11909b0734bc0e00fb2c3d1116
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2021-05-27 23:40:09 -0300

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,12 @@
-import { NgModule } from '@angular/core';
+import { HomeModule } from './../@pages/home/home.module';
+import { AppRoutingModule } from './app-routing-module';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { registerLocaleData } from '@angular/common';
+import localeBr from '@angular/common/locales/pt';
+registerLocaleData(localeBr, 'pt');
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +15,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule, BrowserAnimationsModule,
+    HomeModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
