78fe871d4bee232af49db5820b8df4a0d4900bff
Lucas Copque <lucas.copque@yabaconsultoria.com.br>
2021-05-27 23:41:45 -0300

    add home banner-hero component

---
+++ src/@pages/home/banner-hero/banner-hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-banner-hero',
+  templateUrl: './banner-hero.component.html',
+  styleUrls: ['./banner-hero.component.css']
+})
+export class BannerHeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
